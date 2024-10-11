560c6ab2f02cebbae2c92ea8b3337a0a1b276ee9
waalexan <walteralexandresantanag@gmail.com>
2024-10-11 04:45:17 -0400

    ajustes

--- scrips/player.gd
+++ scrips/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animation := $anim as AnimatedSprite2D
+var is_jumping := false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,13 +15,23 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		is_jumping = true 
+	elif is_on_floor():
+		is_jumping = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
 	if direction:
 		velocity.x = direction * SPEED
+		animation.scale.x = direction
+		if !is_jumping:
+			animation.play("run")
+	elif is_jumping:
+			animation.play("jump")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play('Adle')
 
 	move_and_slide()
